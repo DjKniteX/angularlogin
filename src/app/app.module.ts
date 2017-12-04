4ae2352569e87961d7b87c4469d9bf3ea86e9caf
Nikki X <djknitex@outlook.com>
2017-12-04 08:51:58 -0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+
+const appRoutes: Routes = [
+  { path: 'signup', component: SignupComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '',
+  redirectTo: '/login',
+  pathMatch: 'full'
+},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule
   ],
   providers: [],
